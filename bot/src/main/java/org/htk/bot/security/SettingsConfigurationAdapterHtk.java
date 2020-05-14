70eeb762ec47498b7a476a0f9fb3aee574162999
osvaldoairon <osvaldo_cavalcanti_filho@hotmail.com>
2020-05-14 15:03:19 -0300

    retirando_autenticao_urls_livres

--- bot/src/main/java/org/htk/bot/security/SettingsConfigurationAdapterHtk.java
+++ bot/src/main/java/org/htk/bot/security/SettingsConfigurationAdapterHtk.java
@@ -1,7 +1,7 @@
 package org.htk.bot.security;
 
 
-import org.htk.bot.configuration.JwtRequestFilter;
+
 
 import org.htk.bot.configuration.MyDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,78 +22,58 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import org.htk.bot.configuration.JwtAuthenticationEntryPoint;
+
 
 @Configuration
 @EnableWebSecurity
 public class SettingsConfigurationAdapterHtk 
 	extends WebSecurityConfigurerAdapter{
-	
+
+	/**
 	@Autowired
     private MyDetailService userDetailsService;
+	 * 
+	 */
 	
-	@Autowired
-	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-	@Autowired
-	private UserDetailsService jwtUserDetailsService;
-
-	@Autowired
-	private JwtRequestFilter jwtRequestFilter;
 	
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		
-		httpSecurity.cors().and().csrf().disable().authorizeRequests()
-		.antMatchers("/").permitAll()
-		.antMatchers("/swagger-ui.html").permitAll()
-		.antMatchers("/api/v1/create/").permitAll()
-		.antMatchers(HttpMethod.POST, "/authenticate").permitAll()
-		.anyRequest().authenticated()
-		.and().
-		exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+		httpSecurity.cors().and().csrf().disable();
+		
+		
 		
 	}
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
-	// configure AuthenticationManager so that it knows from where to load
-
-	// user for matching credentials
-
-	// Use BCryptPasswordEncoder
-
-	auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
-
-	}
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	/**
+	 * protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
-		  auth.userDetailsService(userDetailsService);
+	//	  auth.userDetailsService(userDetailsService);
 	//	  System.err.println((userDetailsService.loadUserByUsername("osvaldo")));
 	
-		/**
-		 *auth.inMemoryAuthentication().
-		  withUser("osvaldo").password("{noop}123").roles("USER") .and().
-		  withUser("admin").password("{noop}password").roles("ADMIN"); 
-		 */
-		  
-	}
+		
+		// auth.inMemoryAuthentication().
+		 // withUser("osvaldo").password("{noop}123").roles("USER") .and().
+		 // withUser("admin").password("{noop}password").roles("ADMIN"); 
+		   
+}
+	 */
+	
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 	    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 	
-	@Bean
+	/**
+	 * @Bean
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 	return super.authenticationManagerBean();
 
 	}
+	 */
+	
 	
 	
 	 
